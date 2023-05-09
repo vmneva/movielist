42cdca74f0d1e98fbdd7d216f6089f77af45b320
Viivi Nevalainen <vmneva@utu.fi>
2023-05-09 21:01:24 +0300

    final changes

--- movielist_backend/controllers/users.js
+++ movielist_backend/controllers/users.js
@@ -3,11 +3,16 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    //käytetään populatea ja määritellään näkyviksi blogista otsikko, url sekä kirjoittaja
-    const users = await User
-        .find({}).populate('movies', { name: 1, type: 1} )
-    response.json(users)
-  })
+  //käytetään populatea ja määritellään näkyviksi blogista otsikko, url sekä kirjoittaja
+  const users = await User
+      .find({}).populate('movies', { name: 1, type: 1} )
+  response.json(users)
+})
+
+usersRouter.delete('/:id', async (request, response) => {
+  await User.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
